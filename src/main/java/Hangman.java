4cc7f13318a13a68b16a64b80ad356534817432e
two8g <395120985@qq.com>
2017-11-20 16:04:31 +0800

    add win game_over

--- src/main/java/Hangman.java
+++ src/main/java/Hangman.java
@@ -1,4 +1,64 @@
-package PACKAGE_NAME;
-
 public class Hangman {
+
+    private int tries = 12;
+    private String view;
+    private String used;
+    private String word;
+
+    public Hangman() {
+    }
+
+    public void start(String word) {
+        this.word = word;
+        used = "AEIOU";
+        updateView();
+    }
+
+    public int tries() {
+        return tries;
+    }
+
+    public int length() {
+        return word.length();
+    }
+
+    public String used() {
+        return used;
+    }
+
+    public String view() {
+        return view;
+    }
+
+    public String solution() {
+        return word;
+    }
+
+    public boolean tryChar(char c) {
+        used += c;
+        boolean matched = isMatched(c);
+        if (matched) {
+            updateView();
+        }
+        if (!matched) {
+            tries -= 1;
+        }
+        return matched;
+    }
+
+    private void updateView() {
+        view = word.replaceAll("[^" + used + "]", "_");
+    }
+
+    private boolean isMatched(char c) {
+        return word.indexOf(c) > -1;
+    }
+
+    public boolean win() {
+        return true;
+    }
+
+    public boolean gameOver() {
+        return tries == 0;
+    }
 }
--- src/test/java/HangmanTest.java
+++ src/test/java/HangmanTest.java
@@ -1,2 +1,96 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class HangmanTest {
+    @Test
+    public void should_game_start() {
+        //give
+        Hangman hangman = new Hangman();
+        String word = "WORD";
+        //when
+        hangman.start(word);
+        //then
+        Assert.assertEquals(12, hangman.tries());
+        Assert.assertEquals(4, hangman.length());
+        Assert.assertEquals("AEIOU", hangman.used());
+        Assert.assertEquals("_O__", hangman.view());
+        Assert.assertEquals(word, hangman.solution());
+    }
+
+    @Test
+    public void should_game_start_2() {
+        //give
+        Hangman hangman = new Hangman();
+        String word = "HELLO";
+        //when
+        hangman.start(word);
+        //then
+        Assert.assertEquals(12, hangman.tries());
+        Assert.assertEquals(5, hangman.length());
+        Assert.assertEquals("AEIOU", hangman.used());
+        Assert.assertEquals("_E__O", hangman.view());
+        Assert.assertEquals(word, hangman.solution());
+    }
+
+    @Test
+    public void should_try_correct() {
+        //give
+        Hangman hangman = new Hangman();
+        String word = "WORD";
+        hangman.start(word);
+        //when
+        boolean correct = hangman.tryChar('W');
+        //then
+        Assert.assertTrue(correct);
+        Assert.assertEquals(12, hangman.tries());
+        Assert.assertEquals("AEIOUW", hangman.used());
+        Assert.assertEquals("WO__", hangman.view());
+    }
+
+    @Test
+    public void should_try_incorrect() {
+        //give
+        Hangman hangman = new Hangman();
+        String word = "WORD";
+        hangman.start(word);
+        //when
+        boolean incorrect = hangman.tryChar('A');
+        //then
+        Assert.assertFalse(incorrect);
+        Assert.assertEquals(11, hangman.tries());
+        Assert.assertEquals("AEIOUA", hangman.used());
+        Assert.assertEquals("_O__", hangman.view());
+    }
+
+    @Test
+    public void should_win() {
+        //give
+        Hangman hangman = new Hangman();
+        String word = "WORD";
+        hangman.start(word);
+        //when
+        hangman.tryChar('W');
+        hangman.tryChar('D');
+        hangman.tryChar('R');
+        //then
+        Assert.assertTrue(hangman.win());
+        Assert.assertEquals(12, hangman.tries());
+        Assert.assertEquals("AEIOUWDR", hangman.used());
+        Assert.assertEquals(word, hangman.view());
+    }
+
+    @Test
+    public void should_game_over(){
+        //give
+        Hangman hangman = new Hangman();
+        String word = "WORD";
+        hangman.start(word);
+        //when
+        for (int i = 0; i < 12; i++) {
+            hangman.tryChar('A');
+        }
+        //then
+        Assert.assertTrue(hangman.gameOver());
+        Assert.assertEquals(0, hangman.tries());
+    }
 }
